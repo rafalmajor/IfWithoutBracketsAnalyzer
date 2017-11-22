c599fe5c6d6433c5aabe6a2b241ba230a38a1516
rafalmajor <rafal.major@gmail.com>
2017-11-22 22:20:16 +0100

    update

--- IfWithoutBracketsAnalyzer/IfWithoutBracketsAnalyzer/CodeFixProvider.cs
+++ IfWithoutBracketsAnalyzer/IfWithoutBracketsAnalyzer/CodeFixProvider.cs
@@ -18,7 +18,7 @@ namespace IfWithoutBracketsAnalyzer
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(IfWithoutBracketsAnalyzerCodeFixProvider)), Shared]
     public class IfWithoutBracketsAnalyzerCodeFixProvider : CodeFixProvider
     {
-        private const string title = "Make uppercase";
+        private const string title = "Make If with brackets";
 
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
@@ -27,47 +27,32 @@ namespace IfWithoutBracketsAnalyzer
 
         public sealed override FixAllProvider GetFixAllProvider()
         {
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
             return WellKnownFixAllProviders.BatchFixer;
         }
 
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var node = root.FindNode(context.Span);
 
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
-            var diagnostic = context.Diagnostics.First();
-            var diagnosticSpan = diagnostic.Location.SourceSpan;
+            var expression = node as ExpressionStatementSyntax;
+            if (expression == null)
+            {
+                return;
+            }
 
-            // Find the type declaration identified by the diagnostic.
-            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
+            var action = CodeAction.Create(title, c => AddBrackest(root, expression, context));
 
-            // Register a code action that will invoke the fix.
-            context.RegisterCodeFix(
-                CodeAction.Create(
-                    title: title,
-                    createChangedSolution: c => MakeUppercaseAsync(context.Document, declaration, c),
-                    equivalenceKey: title),
-                diagnostic);
+            context.RegisterCodeFix(action, context.Diagnostics.First());
         }
 
-        private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        private Task<Document> AddBrackest(SyntaxNode root, ExpressionStatementSyntax expression, CodeFixContext context)
         {
-            // Compute new uppercase name.
-            var identifierToken = typeDecl.Identifier;
-            var newName = identifierToken.Text.ToUpperInvariant();
+            var block = SyntaxFactory.Block(expression);
 
-            // Get the symbol representing the type to be renamed.
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
-
-            // Produce a new solution that has all references to that type renamed, including the declaration.
-            var originalSolution = document.Project.Solution;
-            var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
-
-            // Return the new solution with the now-uppercase type name.
-            return newSolution;
+            var newRoot = root.ReplaceNode(expression, block);
+            var newDocument = context.Document.WithSyntaxRoot(newRoot);
+            return Task.FromResult(newDocument);
         }
     }
 }
--- IfWithoutBracketsAnalyzer/IfWithoutBracketsAnalyzer/DiagnosticAnalyzer.cs
+++ IfWithoutBracketsAnalyzer/IfWithoutBracketsAnalyzer/DiagnosticAnalyzer.cs
@@ -15,8 +15,6 @@ namespace IfWithoutBracketsAnalyzer
     {
         public const string DiagnosticId = "IfWithoutBracketsAnalyzer";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
@@ -28,24 +26,18 @@ namespace IfWithoutBracketsAnalyzer
 
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSyntaxNodeAction(AnalyzeNode, ImmutableArray.Create(SyntaxKind.IfStatement));
         }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext nodeContext)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+            var ifStatementSyntax = nodeContext.Node as IfStatementSyntax;
 
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            if (ifStatementSyntax?.Statement is ExpressionStatementSyntax)
             {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
-                context.ReportDiagnostic(diagnostic);
-            }
+                var diagnostic = Diagnostic.Create(Rule, ifStatementSyntax.Statement.GetLocation());
+                nodeContext.ReportDiagnostic(diagnostic);
+            }            
         }
     }
 }
